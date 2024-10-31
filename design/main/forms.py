0b73fb5ce85b7ea10553713dfd048664a2b1629a
Kskskfi <ogyrhik321@gmail.com>
2024-10-31 12:40:27 +0700

    style

--- design/main/forms.py
+++ design/main/forms.py
@@ -8,7 +8,11 @@ class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
-
+    username = forms.CharField(max_length=30, required=True)
+    password1 = forms.CharField(widget=forms.PasswordInput(), label='Пароль')
+    password2 = forms.CharField(widget=forms.PasswordInput(), label='Подтверждение пароля')
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email', 'username', 'password1', 'password2')
+
+
--- design/main/urls.py
+++ design/main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
