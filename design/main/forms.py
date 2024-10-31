4d1ffbf7dafc54d4be921d3c3c4776b3ee835ec1
Kskskfi <ogyrhik321@gmail.com>
2024-11-01 04:23:10 +0700

    style2

--- design/main/forms.py
+++ design/main/forms.py
@@ -5,14 +5,16 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    first_name = forms.CharField(max_length=30, required=True)
-    last_name = forms.CharField(max_length=30, required=True)
-    username = forms.CharField(max_length=30, required=True)
+    email = forms.EmailField(required=True, label='Почта')
+    first_name = forms.CharField(max_length=30, required=True, label='Имя')
+    last_name = forms.CharField(max_length=30, required=True, label='Фамилия')
+    username = forms.CharField(max_length=30, required=True, label='Имя пользователя')
     password1 = forms.CharField(widget=forms.PasswordInput(), label='Пароль')
     password2 = forms.CharField(widget=forms.PasswordInput(), label='Подтверждение пароля')
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email', 'username', 'password1', 'password2')
+
+
 
 
