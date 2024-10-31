b0769fbcae5a22d343838d9a8bca1d8906433c54
Kskskfi <ogyrhik321@gmail.com>
2024-10-31 09:33:36 +0700

    reg_lin_lout

--- design/design/settings.py
+++ design/design/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
+    'main',
 ]
 
 MIDDLEWARE = [
